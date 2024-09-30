6ffaa08ead1cbd5ea54ba22ad261bf77c6f76f18
Manikanta8887 <swamimanikanta66@gmail.com>
2024-09-30 21:46:24 +0530

    Adding Files

---
+++ index.js
@@ -0,0 +1,130 @@
+// Conditional Statements and Control Statements
+
+//  1st Question
+
+// for loop
+
+var king="Pawan Kalyan";
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+ console.log(king);
+
+// 1st Question while loop
+
+var j=1;
+while(j<=10){
+    console.log(j);
+    j++;
+}
+
+console.log(king);
+
+// 1st Question do while
+
+var k=1;
+do{
+    console.log(k);
+    k++;
+}while(k<=10);
+
+console.log(king);
+
+//  2nd Question for loop
+
+for(var e=1;e<=20;e++){
+    if(e%2==0){
+        console.log(e);
+    }
+}
+
+console.log(king);
+
+//  2nd Question while loop
+
+var e1=1;
+
+while(e1<=20){
+    if(e1%2==0){
+    console.log(e1);
+    }e1++;
+}
+
+console.log(king);
+
+
+//  2nd Question do while loop
+
+
+var e2=1;
+do{
+    if(e2%2==0){
+        console.log(e2);
+    }e2++;
+}while(e2<=20);
+
+console.log(king);
+
+//  3rd Question for loop
+
+for(var o=1;o<=20;o++){
+    if(o%2==1){
+        console.log(o);
+    }
+}
+
+console.log(king);
+
+//  3rd Question while loop
+
+var o1=1;
+
+while(o1<=20){
+    if(o1%2==1){
+    console.log(o1);
+    }o1++;
+}
+
+console.log(king);
+
+
+//  3rd Question do while loop
+
+
+var o2=1;
+do{
+    if(o2%2==1){
+        console.log(o2);
+    }o2++;
+}while(o2<=20);
+
+console.log(king);
+
+//  4th Question 
+
+var marks=40;
+
+if(marks>=95 && marks<=85){
+    console.log("O Grade");
+}
+else if(marks<85 && marks>=75){
+    console.log("S Grade");
+}
+else if(marks<75 && marks>=65){
+    console.log("A Grade");
+}
+else if(marks<65 && marks>=55){
+    console.log("B Grade");
+}
+else if(marks<55 && marks>=45){
+    console.log("C Grade");
+}
+else if(marks<45 && marks>=35){
+    console.log("D Grade");
+}
+else{
+    console.log("E Grade");
+}
+
+console.log(king);
